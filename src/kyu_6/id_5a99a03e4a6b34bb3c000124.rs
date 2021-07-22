6134a8bc42155dcc1d3e7045507e423badd2e1f3
oliverdd <oliverdd@qq.com>
2021-07-22 22:15:21 +0800

    Finally

--- src/kyu_6/id_5a99a03e4a6b34bb3c000124.rs
+++ src/kyu_6/id_5a99a03e4a6b34bb3c000124.rs
@@ -1,10 +1,42 @@
-fn num_primorial(n: usize) -> u64 {
-    // TODO
+fn is_prime(n: usize) -> bool {
+    if n <= 1 {
+        return false;
+    }
+    let nl = (n as f64).sqrt() as usize;
+    for i in 2..=nl {
+        if n % i == 0 {
+            return false;
+        }
+    }
+    return true;
+}
+
+fn num_primorial(mut n: usize) -> u64 {
+    let mut result = 2_u64;
+    let mut i = 3;
+    while n > 1 {
+        if is_prime(i) {
+            result *= i as u64;
+            n -= 1;
+        }
+        i += 2;
+    }
+    result
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_is_prime() {
+        assert_eq!(is_prime(2), true);
+        assert_eq!(is_prime(3), true);
+        assert_eq!(is_prime(4), false);
+        assert_eq!(is_prime(5), true);
+        assert_eq!(is_prime(8), false);
+        assert_eq!(is_prime(9), false);
+    }
 
     #[test]
     fn test_basic() {
@@ -14,4 +46,5 @@ mod tests {
         assert_eq!(num_primorial(8), 9699690);
         assert_eq!(num_primorial(9), 223092870);
     }
+
 }
