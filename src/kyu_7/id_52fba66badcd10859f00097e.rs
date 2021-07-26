e3d50e5436cd9457b8b93d467d1fed58e5411fc9
oliverdd <oliverdd@qq.com>
2021-07-27 00:25:01 +0800

    2021/07/25

---
+++ src/kyu_7/id_52fba66badcd10859f00097e.rs
@@ -0,0 +1,13 @@
+fn disemvowel(s: &str) -> String {
+    s.replace(&['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'][..], "")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_test() {
+        assert_eq!(disemvowel("This website is for losers LOL!"), "Ths wbst s fr lsrs LL!");
+    }
+}
